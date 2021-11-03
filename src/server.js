c9a2522c25c8f0da7a11e359c9f8cfae8fb6b4f9
JaeHong94 <moonshin123@naver.com>
2021-11-03 18:53:41 +0900

    ADD: Send and receive messages after connecting.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,27 @@
-import exporess from 'express';
+import http from 'http';
+import WebSocket from 'ws';
+import express from 'express';
 
-const app = exporess();
+const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
-
+app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => res.render('home'));
+app.get('/*', (req, res) => res.redirect('/'));
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (socket) => {
+  console.log('Connected to Browser ✅');
+  socket.on('close', () => console.log('Disconnected to Server ❎'));
+  socket.on('message', (message) => {
+    console.log(message.toString('utf8'));
+  });
+  socket.send('hello!!!');
+});
+
+server.listen(3000, handleListen);
