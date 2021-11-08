d8bdcc6f73f0e8d5b7cdf8090b86b4f1304fd9a6
JaeHong94 <moonshin123@naver.com>
2021-11-08 18:13:43 +0900

    ADD: If you go into the chat room, print out the name of the chat room.

--- src/server.js
+++ src/server.js
@@ -14,11 +14,12 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on('connection', (socket) => {
-  socket.on('enter_room', (msg, done) => {
-    console.log(msg);
-    setTimeout(() => {
-      done('hello from the backend');
-    }, 3000);
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
+  })
+  socket.on('enter_room', (roomName, done) => {
+    socket.join(roomName);
+    done();
   });
 });
 
