76878af242f7a7883a31fe5dabd22189bd9dd8ea
JaeHong94 <moonshin123@naver.com>
2021-11-15 23:02:48 +0900

    ADD: Let's print out the number of people in the chat room.

--- src/server.js
+++ src/server.js
@@ -14,6 +14,8 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 function publicRooms() {
+  // wsServer.sockets.adapter.sids
+  // wsServer.sockets.adapter.rooms
   const {
     sockets: {
       adapter: { sids, rooms }
@@ -28,6 +30,10 @@ function publicRooms() {
   return publicRooms;
 }
 
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 wsServer.on('connection', (socket) => {
   socket['nickname'] = 'Anon';
   socket.onAny((event) => {
@@ -36,15 +42,17 @@ wsServer.on('connection', (socket) => {
   socket.on('enter_room', (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit('welcome', socket.nickname);
+    socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
     wsServer.sockets.emit('room_change', publicRooms());
   });
   socket.on('disconnecting', () => {
-    socket.rooms.forEach(room => socket.to(room).emit('bye', socket.nickname));
+    socket.rooms.forEach(room => {
+      socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1);
+    });
   });
   socket.on('disconnect', () => {
     wsServer.sockets.emit('room_change', publicRooms());
-  })
+  });
   socket.on('new_message', (msg, room, done) => {
     socket.to(room).emit('new_message', `${ socket.nickname }: ${ msg }`);
     done();
